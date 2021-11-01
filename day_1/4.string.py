6a02ecc2f5b6ef4e80c2df12724c7d693776337d
egoing <egoing@gmail.com>
2021-11-01 22:16:33 +0900

    숫자 데이터 타입과 연산자들

---
+++ day_1/4.string.py
@@ -0,0 +1,14 @@
+# 문자열 STRING
+print('Hello World')
+print("Hello WOrld")
+print("Hello \nWorld")
+print("Hello \"W\"orld")
+print("""
+H
+E
+L
+L
+O
+""")
+
+print('Hi, Hell World'.replace('Hell', 'Hello'))
