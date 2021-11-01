7f0d5435e18922a9c25b07def164d5eba8d9bfa3
egoing <egoing@gmail.com>
2021-11-01 22:27:08 +0900

    리스트 데이터 타입 수업 예제

---
+++ day_1/5.list.py
@@ -0,0 +1,13 @@
+#  값을 원소(element), 색인(index)
+members = ['egoing','leezche','blackdew']
+print(members[0])
+print(members[2])
+print(len(members))
+
+import random
+print(random.choice(members))
+
+score = [100, 80, 10]
+print(min(score))
+print(max(score))
+print(sum(score))
