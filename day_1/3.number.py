870ee9ec544119ae973f9d9746701739c5d6df1a
egoing <egoing@gmail.com>
2021-11-01 22:02:58 +0900

    숫자 데이터 타입을 위한 예제

---
+++ day_1/3.number.py
@@ -0,0 +1,14 @@
+print(1+1) #2
+print(4-2) #3
+print(4*4) #16
+print(4/2) #2
+
+print(pow(3,2))
+
+import math
+print(math.sqrt(16))
+
+import random
+print(math.floor(random.random()*100))
+
+print(random.randrange(100,200))
