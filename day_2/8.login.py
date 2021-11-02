3763b5beb02a2cdc757adb0119b23cdb3df83ecc
egoing <egoing@gmail.com>
2021-11-02 21:07:54 +0900

    elif를 이용해서 개선

--- day_2/8.login.py
+++ day_2/8.login.py
@@ -1,11 +1,13 @@
-id = input('id ?')
+# id = input('id ?')
 # if id == 'egoing':
 #     print('Welcome')    
 # if id != 'egoing':
 #     print('WHO ARE YOU!!!?')
 
 if id == 'egoing':
-    print('Welcome')    
+    print('Welcome')  
+elif id == 'leezche':
+    print('Welcome')
 else:
     print('WHO ARE YOU!!!?')
     
